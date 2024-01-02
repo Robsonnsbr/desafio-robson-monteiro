99374b38cdb8c8849ca218dae4275c6de6517f2f
robsonnsbr <robsonnsbr@gmail.com>
2024-01-02 18:41:54 -0300

    fix: text tooltip

--- front-end/src/components/common/ToolTip.tsx
+++ front-end/src/components/common/ToolTip.tsx
@@ -10,10 +10,10 @@ const Tooltip: React.FC<TooltipProps> = ({ children, text }) => {
   let style = '';
   if (name === 'ButtonOpenModal') {
     style =
-      'absolute bg-customBlack text-white p-2 rounded-md opacity-0 invisible transition-opacity duration-300 ease-in-out group-hover:opacity-100 group-hover:visible sm:group-hover:invisible';
+      ' absolute right-20 -top-10 bg-customBlack text-white p-2 rounded-md opacity-0 invisible transition-opacity duration-300 ease-in-out group-hover:opacity-100 group-hover:visible sm:group-hover:invisible';
   } else {
     style =
-      'absolute bg-customBlack text-white p-2 rounded-md opacity-0 invisible transition-opacity duration-300 ease-in-out group-hover:opacity-100 group-hover:visible';
+      ' absolute right-20 -top-10 bg-customBlack text-white p-2 rounded-md opacity-0 invisible transition-opacity duration-300 ease-in-out group-hover:opacity-100 group-hover:visible';
   }
 
   return (
